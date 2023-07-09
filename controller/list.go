cb9c15386279fe5527426e0ffa985b93815cfac7
Poul-george <65060101+Poul-george@users.noreply.github.com>
2023-07-09 20:35:35 +0900

    tweak

--- controller/list.go
+++ controller/list.go
@@ -1,11 +1,12 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/Poul-george/go-render-ap/go-render-api/response"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func List(ctx echo.Context) error {
-	return response.ListRes(ctx, http.StatusOK, nil)
+	return response.ListRes(ctx, http.StatusOK, "text")
 }
--- response/response.go
+++ response/response.go
@@ -4,13 +4,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func ListRes(c echo.Context, code int, recipes interface{}) error {
+func ListRes(c echo.Context, code int, text interface{}) error {
 	return c.JSON(
 		code,
 		struct {
-			Recipes interface{} `json:"recipes"`
+			Text interface{} `json:"text"`
 		}{
-			Recipes: recipes,
+			Text: text,
 		},
 	)
 }
